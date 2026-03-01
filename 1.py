577e0dd272d54ffa8c944f7fa7e58dc6ce7f420a
Shaik Sohail <shaikxsohail1@gmail.com>
2026-03-01 23:16:30 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,6 +1,27 @@
-phone = input("Enter phone number: ")
-
-if phone.isdigit() and len(phone) == 10:
-    print("Valid phone number")
-else:
-    print("Invalid phone number")
+d={}
+n=int(input("enter nuber of students: "))
+for i in range(n):
+    n=input("n:")
+    m=[]
+    for i in range(5):
+        ma=int(input("mar: "))
+        m.append(ma)
+    total=sum(m)
+    avg=total/5
+    if avg>=90:
+        grade="A"
+    elif avg>=70 and avg<=90:
+        grade="B"
+        
+    else:
+        grade="Fail"
+    d[n]={
+        "marks":m,
+        "total":total,
+        "avg":avg,
+        "grade":grade
+    }
+topper=max(d,key=lambda x: d[x]["total"])
+for k,v in d.items():
+    print(k,v)
+print("topper: ",topper)
